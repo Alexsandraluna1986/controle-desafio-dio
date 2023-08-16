1c783f96e6821d7affd0140eaf687aa97d631f4f
Alexsandra Luna <alexsandraluna1@gmail.com>
2023-08-16 16:33:48 -0300

    novo desafio

---
+++ eclipse-workspace/DesafioControleFluxo/src/application/ParametrosInvalidosException.java
@@ -0,0 +1,13 @@
+package application;
+
+public class ParametrosInvalidosException extends Exception{
+	
+	public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+
+	public ParametrosInvalidosException() {
+	}
+	
+
+}
---
+++ eclipse-workspace/DesafioControleFluxo/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author alexsandra
+ *
+ */
+module DesafioControleFluxo {
+}
